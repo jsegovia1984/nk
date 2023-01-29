99e3079be0686e2069b985df437a51a909d080df
GiFerDel <giferdel@gmail.com>
2023-01-29 13:47:34 -0300

    duplicado funcion menu2

--- menudigital/views.py
+++ menudigital/views.py
@@ -14,7 +14,7 @@ class menu(ListView):
     model = postres
     template_name = 'menu.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(self, **kwargs)
-        context['postres'] =  postres.objects.all()
-        return context
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(self, **kwargs)
+    #     context['postres'] =  postres.objects.all()
+    #     return context
